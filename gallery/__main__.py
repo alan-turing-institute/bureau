c771094e30dcbcc78c16ec08c4d0ff878abe39f6
Jim Madge <jim@jmadge.com>
2022-11-03 16:08:31 +0000

    Improve image definition declaration

--- gallery/__main__.py
+++ gallery/__main__.py
@@ -1,5 +1,4 @@
 import pulumi
-from pulumi import ComponentResource, ResourceOptions
 from pulumi_azure_native import compute, resources
 
 # Create an Azure Resource Group
@@ -14,50 +13,18 @@ gallery = compute.Gallery(
     resource_group_name=resource_group.name
 )
 
-
-class ImageDefinitionArgs:
-    def __init__(
-        self,
-        gallery: compute.Gallery,
-        resource_group: resources.ResourceGroup,
-        sku: str
-    ):
-        self.gallery = gallery
-        self.resource_group = resource_group
-        self.sku = sku
-
-
-class ImageDefinition(ComponentResource):
-    def __init__(self, name: str, args: ImageDefinitionArgs,
-                 opts: ResourceOptions = None):
-        super().__init__("custom:app:ImageDefinition", name, {}, opts)
-        child_opts = ResourceOptions(parent=self)
-
-        compute.GalleryImage(
-            name,
-            resource_group_name=args.resource_group.name,
-            gallery_name=args.gallery.name,
-            identifier=compute.GalleryImageIdentifierArgs(
-                publisher="The_Alan_Turing_Institute",
-                offer="Bureau",
-                sku=args.sku,
-            ),
-            os_state=compute.OperatingSystemStateTypes.GENERALIZED,
-            os_type=compute.OperatingSystemTypes.LINUX,
-            opts=child_opts
-        )
-
-        self.register_outputs({})
-
-
 focal_image_definition, jammy_image_definition = (
-    ImageDefinition(
+    compute.GalleryImage(
         f"bureau_{sku}",
-        ImageDefinitionArgs(
-            gallery=gallery,
-            resource_group=resource_group,
-            sku=sku
-        )
+        resource_group_name=resource_group.name,
+        gallery_name=gallery.name,
+        identifier=compute.GalleryImageIdentifierArgs(
+            publisher="The_Alan_Turing_Institute",
+            offer="Bureau",
+            sku=sku,
+        ),
+        os_state=compute.OperatingSystemStateTypes.GENERALIZED,
+        os_type=compute.OperatingSystemTypes.LINUX,
     )
     for sku in ["focal", "jammy"]
 )
